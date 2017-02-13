a83d1487b4db868537c918cee3a3c736100fa0c0
James Coulson <coulsonj@gmail.com>
2017-02-13 16:24:28 +0000

    First working version

--- arsenal_ticket_exchange.py
+++ arsenal_ticket_exchange.py
@@ -1,12 +1,20 @@
 import json
 import requests
 from pyquery import PyQuery
+from random import randint
 from time import sleep
 
-#event_id = '6474' # Bayern
-event_id = '6462' # Hull
+
+
+event_id = '6474' # Bayern
+#event_id = '6477' # Leicester
 form_url = 'https://www.eticketing.co.uk/arsenal/Authentication/Login/Process'
 event_url = 'https://www.eticketing.co.uk/arsenal/details/event.aspx?itemref=' + event_id
+
+proxies={
+    'http': 'http://127.0.0.1:8080',
+    'https': 'https://127.0.0.1:8080'
+}
 
 payload = {
     'returnUrl': '/arsenal/',
@@ -15,9 +23,12 @@ payload = {
     'SecondaryCredential': 'arsenal'
 }
 
+
+
+
 # Login
 s = requests.Session()
-login = s.post(form_url, data=payload)
+login = s.post(form_url, data=payload, proxies=proxies, verify=False)
 event_response = s.get(event_url)
 event_page = PyQuery(event_response.text)
 
@@ -32,16 +43,8 @@ post_url = 'https://www.eticketing.co.uk/arsenal/details/event.aspx?itemref={eve
 
 
 
-# Select the best available tab
 
-headers = {
-    'Host': 'www.eticketing.co.uk',
-    'Connection': 'keep-alive',
-    'Origin': 'https://www.eticketing.co.uk',
-    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'Accept': '*/*',
-    'Referer': event_url
-}
+# Select the ticket exchange part
 
 post_data = {
     'Anthem_UpdatePage': 'true',
@@ -51,50 +54,76 @@ post_data = {
     '__VIEWSTATEGENERATOR': 'B7FC5CAC'
 }
 
-r = s.post(post_url, data=post_data, headers=headers)
+r = s.post(post_url, data=post_data, proxies=proxies, verify=False)
 ticket_response = json.loads(r.text)
-post_data['view_state'] = ticket_response['viewState']
-#s.cookies.set('_gali','ctl00_body_seatselection1_selectionmodes1_repeaterModes_ctl02_ctl02')
+view_state = ticket_response['viewState']
 
 
 # Select the Ticket Exchange best available
-post_data['__EVENTTARGET'] = 'ctl00$body$seatselection1$bestavailable1$fliptoback'
 
-#post_data = {
-#    'Anthem_UpdatePage': 'true',
-#    '__EVENTTARGET': 'ctl00$body$seatselection1$bestavailable1$fliptoback',
-#    '__EVENTARGUMENT': '',
-#    #'__VIEWSTATE': view_state,
-#    '__VIEWSTATEGENERATOR': 'B7FC5CAC'
-#}
-
-r = s.post(post_url, data=post_data, headers=headers)
-ticket_response = json.loads(r.text)
-print(ticket_response['controls'].keys())
-print(ticket_response)
-post_data['view_state'] = ticket_response['viewState']
-response_html = PyQuery(ticket_response['controls']['ctl00$body$seatselection1$bestavailable1$panelflip'])
-price_class = response_html('#ctl00_body_seatselection1_bestavailable1_repeaterPriceClassesTX_ctl00_hiddenPriceClassTX').attr('value')
 
 post_data = {
     'Anthem_UpdatePage': 'true',
-    '__EVENTTARGET': 'ctl00%24body%24seatselection1%24bestavailable1%24cmdAddToBasketTX',
+    '__EVENTTARGET': 'ctl00$body$seatselection1$bestavailable1$fliptoback',
+    '__EVENTARGUMENT': '',
+    '__VIEWSTATE': view_state,
+    '__VIEWSTATEGENERATOR': 'B7FC5CAC'
+}
+
+r = s.post(post_url, data=post_data, proxies=proxies, verify=False)
+ticket_response = json.loads(r.text)
+view_state = ticket_response['viewState']
+response_html = PyQuery(ticket_response['controls']['ctl00$body$seatselection1$bestavailable1$panelflip'])
+price_class = response_html('#ctl00_body_seatselection1_bestavailable1_repeaterPriceClassesTX_ctl00_hiddenPriceClassTX').attr('value')
+
+
+
+# Request a ticket
+
+
+post_data = {
+    'Anthem_UpdatePage': 'true',
+    '__EVENTTARGET': 'ctl00$body$seatselection1$bestavailable1$cmdAddToBasketTX',
     '__EVENTARGUMENT': '',
     '__VIEWSTATE': view_state,
     '__VIEWSTATEGENERATOR': 'B7FC5CAC',
     'ctl00$body$seatselection1$bestavailable1$repeaterPriceClassesTX$ctl00$hiddenPriceClassTX': price_class,
     'ctl00$body$seatselection1$bestavailable1$repeaterPriceClassesTX$ctl00$listQtyTX': '1',
-    'ctl00$body$seatselection1$bestavailable1$repeaterPriceClassesTX$ctl00$minprice': '26.00',
-    'ctl00$body$seatselection1$bestavailable1$repeaterPriceClassesTX$ctl00$maxprice': '162.50'
+    'ctl00$body$seatselection1$bestavailable1$repeaterPriceClassesTX$ctl00$minprice': '0.00',
+    'ctl00$body$seatselection1$bestavailable1$repeaterPriceClassesTX$ctl00$maxprice': '101.50'
 }
 
-r = s.post(post_url, data=post_data)
+r = s.post(post_url, data=post_data, proxies=proxies, verify=False)
 ticket_response = json.loads(r.text)
-
-# Reset the view state
-post_data['view_state'] = ticket_response['viewState']
+view_state = ticket_response['viewState']
 
 # Get the response
-message = PyQuery(ticket_response['controls']['ctl00$body$seatselection1$isc1$panelSeatSelection'])
-print(r.text)
+message = PyQuery(ticket_response['controls']['ctl00$body$seatselection1$bestavailable1$avs1'])
+print(message)
+
+resp_msg = message('ul#errorsummary').children('li')[0].text
+error_msg = 'None of the prices you selected could be found. Please try back later.'
+
+while error_msg == resp_msg:
+    post_data = {
+        'Anthem_UpdatePage': 'true',
+        '__EVENTTARGET': 'ctl00$body$seatselection1$bestavailable1$cmdAddToBasketTX',
+        '__EVENTARGUMENT': '',
+        '__VIEWSTATE': view_state,
+        '__VIEWSTATEGENERATOR': 'B7FC5CAC',
+        'ctl00$body$seatselection1$bestavailable1$repeaterPriceClassesTX$ctl00$hiddenPriceClassTX': price_class,
+        'ctl00$body$seatselection1$bestavailable1$repeaterPriceClassesTX$ctl00$listQtyTX': '1',
+        'ctl00$body$seatselection1$bestavailable1$repeaterPriceClassesTX$ctl00$minprice': '0.00',
+        'ctl00$body$seatselection1$bestavailable1$repeaterPriceClassesTX$ctl00$maxprice': '101.50'
+    }
+    
+    r = s.post(post_url, data=post_data, proxies=proxies, verify=False)
+    ticket_response = json.loads(r.text)
+    view_state = ticket_response['viewState']
+    
+    message = PyQuery(ticket_response['controls']['ctl00$body$seatselection1$bestavailable1$avs1'])
+    print(message)
+    
+    resp_msg = message('ul#errorsummary').children('li')[0].text
+    sleep(randint(1,3))
 
